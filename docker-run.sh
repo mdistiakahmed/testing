6d45826bdf2acbcd9d5f5e0f4157a3cb3f4012b7
istiak.ahmed.enosis <istiak.ahmed.enosis@powerhousedynamics.com>
2025-08-16 22:26:47 +0600

    update cloudwathc onfig

--- docker-run.sh
+++ docker-run.sh
@@ -9,6 +9,7 @@ IMAGE_TAG="latest"
 docker run --rm \
   --name crawlee-container \
   -v "$(pwd):/app" \
-  --memory=1g \
-  --memory-swap=1g \
+  -e AWS_ACCESS_KEY_ID=AKIAS2UEZEAMK3VDZ6PG \
+  -e AWS_SECRET_ACCESS_KEY=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 \
+  -e AWS_REGION=us-east-1 \
   ${IMAGE_NAME}:${IMAGE_TAG}
